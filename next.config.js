8e7084e3c9c7e5c84567835431d4275f3ba61316
Patrick Allan Wood <nuwudorder@gmail.com>
2025-03-13 11:13:35 -0700

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    // Environment variables will be added here
+  },
+};
+
+module.exports = nextConfig;
